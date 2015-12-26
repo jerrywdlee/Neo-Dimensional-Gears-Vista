d65053d8d50ec01bde725da8236aa18e59a6978c
Jerry Lee <lzjjerrywoodylee@gmail.com>
2015-12-27 07:55:50 +0900

    加了个密
    将原先http链接改为了自签名的https

--- WebSocketTest/index.js
+++ WebSocketTest/index.js
@@ -1,11 +1,21 @@
+var fs = require('fs'), //读取RSA加密证书用的文件读取模块
+    rsa_options = {
+        key: fs.readFileSync('./openssl_keys/server_key.pem'), //SSL密钥路径 
+        cert: fs.readFileSync('./openssl_keys/server_crt.pem') //SSL证书路径
+    };
+
 var express = require('express'), //引入express模块
     app = express(),
-    server = require('http').createServer(app);
+    //server = require('http').createServer(app);
+    server = require('https').createServer(rsa_options, app); //使用https连接
     io = require('socket.io').listen(server); //引入socket.io模块并绑定到服务器
 app.use('/', express.static(__dirname + '/www')); //指定静态HTML文件的位置
 
-server.listen(80,function (req, res) {
-  console.log('app is running at port 80');
+
+//server.listen(80,function (req, res) {
+server.listen(443,function (req, res) { //https的默认端口是443
+  //console.log('app is running at port 80');
+  console.log('app is running at port 443');
 });
 
 function getLeg(leg1,leg2){
@@ -15,7 +25,7 @@ function getLeg(leg1,leg2){
 //socket部分
 io.on('connection', function(socket) {
     //接收并处理客户端发送的foo事件
-    console.log('Socket.io listen at port 80')
+    console.log('Socket.io listening')
     socket.on('foo', function(data1,data2) {
         //将消息输出到控制台
         console.log('a='+data1);
